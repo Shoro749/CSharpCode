5ee9c5b1681ccc7e32187b7c635ea79829193631
Shoro749 <romanbro749@gmail.com>
2023-08-19 14:23:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace CSharpCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.InputEncoding = Encoding.Unicode;
+                Console.OutputEncoding = Encoding.Unicode;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Помилка! {ex.Message}");
+            }
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
