d5f0e6218387360848e237122c6e46af55d5a063
Shoro749 <romanbro749@gmail.com>
2023-08-19 17:43:29 +0300

    N

---
+++ IMusicInstruments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCode
+{
+    public interface IMusicInstruments
+    {
+        void Sound();
+        void History();
+    }
+}
