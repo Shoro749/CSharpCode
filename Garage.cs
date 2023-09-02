80b9a24cbf168274d1174f601e6967a62ab527ac
Shoro749 <romanbro749@gmail.com>
2023-09-02 16:18:17 +0300

    N

---
+++ Garage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Створіть клас «Гараж». Клас має містити інформацію
+про машини, які знаходяться в гаража. Створіть клас
+«Автомобіль», в якому, відповідно, міститься інформація
+про автомобіль. Реалізуйте підтримку ітератора для класу
+«Гараж». Протестуйте можливість використання foreach
+для класу «Гараж».*/
+
+namespace CSharpCode
+{
+    public class Garage
+    {
+        private List<int> cars = new List<int>();
+    }
+}
---
+++ Сar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Створіть клас «Гараж». Клас має містити інформацію
+про машини, які знаходяться в гаража. Створіть клас
+«Автомобіль», в якому, відповідно, міститься інформація
+про автомобіль. Реалізуйте підтримку ітератора для класу
+«Гараж». Протестуйте можливість використання foreach
+для класу «Гараж».*/
+
+namespace CSharpCode
+{
+    public class Сar
+    {
+        public string _Brand;
+        public string _color;
+        public string _FuelType;
+        public int _EngineCapacity;
+        public int _power;
+        public int _revolutions;
+
+        public Сar(string brand, string color, string fuelType, int engineCapacity, int power, int revolutions)
+        {
+            _Brand = brand;
+            _color = color;
+            _FuelType = fuelType;
+            _EngineCapacity = engineCapacity;
+            _power = power;
+            _revolutions = revolutions;
+        }
+    }
+}
