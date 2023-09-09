1a71e2ade6b54a1461769daea1d809cb40d68c7c
Shoro749 <romanbro749@gmail.com>
2023-09-09 17:36:54 +0300

    Didnt end

---
+++ Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCode
+{
+    public class Car
+    {
+        public string Name { get; set; }      
+        public string Manufacturer { get; set; } 
+        public int Year { get; set; }           
+
+        public Car(string name, string manufacturer, int year)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Year = year;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,28 @@ namespace CSharpCode
         {
             try
             {
+                /*Створіть користувацький тип «Автомобіль», який зберігатиме
+                таку інформацію:
+                 назва;
+                 виробник;
+                 рік випуску.
+                Для двох масивів автомобілів реалізуйте операції:
+                 різниця масивів;
+                 перетин масивів;
+                 об'єднання масивів.
+                Критерій для виконання операцій – виробник.*/
+
                 Console.InputEncoding = Encoding.Unicode;
                 Console.OutputEncoding = Encoding.Unicode;
+
+                Car car1 = new Car("Car", "Ford", 2010);
+                Car car2 = new Car("Car_2.0", "Honda", 2007);
+                Car car3 = new Car("Car_1.0", "Fiat-Chrysler", 2016);
+                Car car4 = new Car("Car_1", "Honda", 2000);
+                Car car5 = new Car("Car_", "BMW", 2020);
+                Car car6 = new Car("Car_2.0", "Ford", 2016);
+                Car[] cars1 = { car1, car2, car3 };
+                Car[] cars2 = { car4, car5, car6 };
             }
             catch (Exception ex)
             {
